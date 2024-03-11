4a7403f362690cbdbb8b0bb86a9368160144046c
Kruize Derek Christensen <christensen.kruize@gmail.com>
2024-03-11 12:18:09 -0600

    Add files via upload

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Kruize Christensen on 3/7/24.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
